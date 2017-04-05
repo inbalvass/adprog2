9c9a216bb642d26307a3131398dc7541c821bab6
miki1 <ibaley@walla.com>
2017-04-05 11:03:48 +0300

    dfs part code

--- SearchAlgorithmsLib/SearchAlgorithmsLib/DFS.cs
+++ SearchAlgorithmsLib/SearchAlgorithmsLib/DFS.cs
@@ -4,8 +4,22 @@ using System.Text;
 
 namespace SearchAlgorithmsLib
 {
-    class DFS<T> : Searcher<T>
+    /* pseudo code
+    1  procedure DFS-iterative(G, v):
+2      let S be a stack
+3      S.push(v)
+4      while S is not empty
+5          v = S.pop()
+6          if v is not labeled as discovered:
+7              label v as discovered
+8              for all edges from v to w in G.adjacentEdges(v) do 
+9                  S.push(w)*/
+    class DFS<T> : ISearcher<T>
     {
-
+        public override Solution<T> search(ISearchable<T> searchable)
+        {
+            Stack<State<T>> stack = new Stack<State<T>>();
+            State<T> node = searchable.getInitialState();
+        }
     }
 }
