06565c5f2adf884c3f4e0d0ae8606eb2ff70e681
miki1 <ibaley@walla.com>
2017-04-06 13:05:42 +0300

    add the dlls and the console app

---
+++ SearchAlgorithmsLib/checksTheDll/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MazeGeneratorLib;
+using MazeLib;
+using SearchAlgorithmsLib;
+
+
+namespace checksTheDll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
