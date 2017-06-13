d7aaa5d2fdfb19382638942247e476a605f5c6ae
hodayaBerdugo <hodaya_dr0@walla.com>
2017-06-13 19:38:27 +0300

    commit

---
+++ SearchAlgorithmsLib/WebServer/Controllers/SinglePlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebServer.Models;
+
+namespace WebServer.Controllers
+{
+    public class SinglePlayerController : ApiController
+    {
+        public string Name = "k";
+        public SinglePlayerModel spm;
+
+        //public SinglePlayerController()
+        //{
+        //    spm = new SinglePlayerModel("k");
+        //}
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        //public IHttpActionResult GetSinglePlayer(string name)
+        //{
+        //    SinglePlayerModel spm = new SinglePlayerModel(name);
+        //    return Ok(spm);
+        //}
+    }
+}
---
+++ SearchAlgorithmsLib/WebServer/Models/SinglePlayerModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebServer.Models
+{
+    public class SinglePlayerModel
+    {
+        public string Name { get; set; }
+
+        public SinglePlayerModel(string name)
+        {
+            Name = name;
+        }
+    }
+}
