25cc32eea4a6b7c5688a36e2612868202ba9786f
hodayaBerdugo <hodaya_dr0@walla.com>
2017-06-16 19:05:26 +0300

    .

--- SearchAlgorithmsLib/WebApplication1/Models/SinglePlayerModel.cs
+++ SearchAlgorithmsLib/WebApplication1/Models/SinglePlayerModel.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using MazeGeneratorLib;
+using MazeLib;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,12 +14,28 @@ namespace WebServer.Models
         public string Name { get; set; }
         public int Rows { get; set; }
         public int Cols { get; set; }
+        //public Maze MyMaze { get; set; }
+       //  public string MazeJSON { get; set; }
+        public string MazeStr { get; set; }
 
-        public SinglePlayerModel(string name,int rows,int colomns)
+        public SinglePlayerModel(string name,int rows,int cols)
         {
             Name = name;
             Rows = rows;
-            Cols = Cols;
+            Cols = cols;
+            //       DFSMazeGenerator mazeGenerate = new DFSMazeGenerator();
+            //        Maze maze = mazeGenerate.Generate(rows, cols);
+            Maze maze = new Maze(rows, cols);
+            maze.Name = name;
+            //   maze[3, 3] = CellType.Free;
+
+            //string MazeJSON = maze.ToJSON();
+            //dynamic data = JsonConvert.DeserializeObject(MazeJSON);
+            ////data["Maze"]
+            //MazeStr = data["Maze"];
+            JObject jmaze = JObject.Parse(maze.ToJSON());
+            MazeStr = jmaze.GetValue("Maze").ToString();
+            
         }
     }
 }
