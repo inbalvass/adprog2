89d310610e7ae936be95b4db4c260813497d7a4a
hodayaBerdugo <hodaya_dr0@walla.com>
2017-06-19 15:20:17 +0300

    added movements

--- SearchAlgorithmsLib/WebApplication1/scripts/jquery.mazeBoard.js
+++ SearchAlgorithmsLib/WebApplication1/scripts/jquery.mazeBoard.js
@@ -9,7 +9,12 @@
         var cellWidth = myCanvas.width / cols;
         var cellHeight = myCanvas.height / rows;
         var counter = 0;
+
+        var currRow = startRow;
+        var currCol = startCol;
+        
         drawMaze();
+        myCanvas.onkeydown = move.bind(this);
         function drawMaze() {
             for (var i = 0; i < rows; i++) {
                 for (var j = 0; j < cols; j++) {
@@ -30,12 +35,64 @@
                     counter++;
                 }
             }
+            myCanvas.focus();
         }
 
-        function move() {
+        function deletePlayer() {
+            context.clearRect(currCol * cellWidth, currRow * cellHeight, cellWidth, cellHeight);
+        };
 
+        function drawPlayer() {
+            context.drawImage(playerImage, currCol * cellWidth, currRow * cellHeight, cellWidth, cellHeight);
+        };
+
+        function move(e) {
+            var newCurrCol = currCol;
+            var newCurrRow = currRow;
+            var newIndexInMaze = indexInMaze;
+            switch (e.keyCode) {
+                case 38:
+                    newCurrRow -= 1;
+                    newIndexInMaze -= cols;
+                    break;
+                case 40:
+                    newCurrRow += 1;
+                    newIndexInMaze += cols;
+                    break;
+                case 39:
+                    newCurrCol += 1;
+                    newIndexInMaze += 1;
+                    break;
+                case 37:
+                    newCurrCol -= 1;
+                    newIndexInMaze -= 1;
+                    break;
+            }
+            //check if the new indexes are legel
+            if ((newCurrCol < cols) && (newCurrRow < rows)
+                && (newCurrCol >= 0) && (newCurrRow >= 0) && maze[newIndexInMaze] == '0') {
+
+                //delete the player
+                deletePlayer();
+
+                //updating new indexes
+                currCol = newCurrCol;
+                currRow = newCurrRow;
+                indexInMaze = newIndexInMaze;
+
+                //draw the player in the new position
+                drawPlayer();
+
+
+                checkIfWin();
+            }
         }
-        
-        
+
+        function checkIfWin() {
+            if (currCol == endCol && currRow == endRow) {
+                //$("#dialog").dialog();
+                alert("Congratulations, You Won!");
+            }
+        }
     };
 })(jQuery);
