45d3e8b9f3a5cfb58dab25fb7515cd9870b61a36
hodayaBerdugo <hodaya_dr0@walla.com>
2017-06-19 11:19:20 +0300

    creating jquery plugin

--- SearchAlgorithmsLib/WebApplication1/scripts/getMaze.js
+++ SearchAlgorithmsLib/WebApplication1/scripts/getMaze.js
@@ -1,34 +1,24 @@
-﻿//var apiUrl = "api/SinglePlayer";
-////var btn = document.getElementById("StartGameBtn");
-//(function ($) {
-//    $("#StartGameBtn").click(function () {
-//        var name = document.getElementById("txtName").value;
-//        var rows = document.getElementById("txtRows").value;
-//        var cols = document.getElementById("txtCols").value;
-//        $.getJSON(apiUrl + "/" + name + "/" + rows + "/" + cols)
-//            .done(function (maze) {
-//                var json = JSON.parse(maze);
-//                //  var theKey = 'Rows';
-//                //alert(json['Rows']);
-//                alert('h');
-//            })
-//            .fail(function (jqXHR, textStatus, err) {
-//                $("#product").text("Error: " + err);
-//            });
-//    });
-//});
+﻿var apiUrl = "api/SinglePlayer";
 function getMaze() {
     var name = document.getElementById("txtName").value;
     var rows = document.getElementById("txtRows").value;
     var cols = document.getElementById("txtCols").value;
-    //$("#loader").show();
+    $("#loader").show();
     $.getJSON(apiUrl + "/" + name + "/" + rows + "/" + cols)
         .done(function (maze) {
             var json = JSON.parse(maze);
-            alert('here');
-           // $("#loader").hide();
-           // $("#div").show();
-            drawMaze(json['Maze'], rows, cols, json['Start']['Row'], json['Start']['Col'], json['End']['Row'], json['End']['Col']);
+            $("#loader").hide();
+            $("#div").show();
+            var playerImage = new Image();
+            playerImage.src = "images/pacman.png";
+            var exitImage = new Image();
+            exitImage.src = "images/exit.jpg";
+            var myMazeBoard = $("#mazeCanvas").mazeBoard(json['Maze'],
+                                                        json['Start']['Row'],
+                                                        json['Start']['Col'],
+                                                        json['End']['Row'],
+                                                        json['End']['Col'],
+                                                        playerImage, exitImage, true);
         })
         .fail(function (jqXHR, textStatus, err) {
             alert("Error: " + err);
---
+++ SearchAlgorithmsLib/WebApplication1/scripts/jquery.mazeBoard.js
@@ -0,0 +1,41 @@
+﻿(function ($) {
+    $.fn.mazeBoard = function (maze, startRow, startCol, endRow, endCol,
+                                playerImage, exitImage, isEnabled) {
+        var rows = parseInt($("#txtRows").val());
+        var cols = parseInt($("#txtCols").val());
+        var myCanvas = this[0];
+        var indexInMaze;
+        var context = myCanvas.getContext("2d");
+        var cellWidth = myCanvas.width / cols;
+        var cellHeight = myCanvas.height / rows;
+        var counter = 0;
+        drawMaze();
+        function drawMaze() {
+            for (var i = 0; i < rows; i++) {
+                for (var j = 0; j < cols; j++) {
+                    if (maze[counter] == '1') {
+                        context.fillRect(cellWidth * j, cellHeight * i, cellWidth, cellHeight);
+                    }
+                    if (i == startRow && j == startCol) {
+                        playerImage.onload = function () {
+                            context.drawImage(playerImage, cellWidth * startCol, cellHeight * startRow, cellWidth, cellHeight);
+                        };
+                        indexInMaze = counter;
+                    }
+                    if (i == endRow && j == endCol) {
+                        exitImage.onload = function () {
+                            context.drawImage(exitImage, cellWidth * endCol, cellHeight * endRow, cellWidth, cellHeight);
+                        };
+                    }
+                    counter++;
+                }
+            }
+        }
+
+        function move() {
+
+        }
+        
+        
+    };
+})(jQuery);
