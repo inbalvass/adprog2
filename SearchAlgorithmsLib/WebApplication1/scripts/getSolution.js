df67be64be458ebed972d73d5ecff4249df3f80f
hodayaBerdugo <hodaya_dr0@walla.com>
2017-06-26 11:04:34 +0300

    finish SOLVE

--- SearchAlgorithmsLib/WebApplication1/scripts/getSolution.js
+++ SearchAlgorithmsLib/WebApplication1/scripts/getSolution.js
@@ -1,32 +1,8 @@
-﻿//src = "/jquery.mazeBoard.js";
-
-function getSolution() {
-    //alert("solution");
-    //$.getScript("jquery.mazeBoard.js", function () {
-
-    //    alert("Script loaded but not necessarily executed.");
-
-    //});
-
+﻿function getSolution() {
+  
     var apiUrl = "api/SinglePlayer";
-    //var name = $("#txtName").val();
     var name = document.getElementById("txtName").value;
     var algo = document.getElementById("algorithm").value;
-    //var algo = $("#algorithm").val();
-
-
-    //after we have db with list of games:
-
-    //$.get(apiUrl, { name: name, algo: algorithm })
-    //        .done(function (solution) {
-    //            $("#mazeCanvas").solveMaze(solution);
-    //        })
-    //        .fail(...) {
-
-    //        });
-
-    //check
-
 
     $.getJSON(apiUrl + "/" + name + "/" + algo)
        .done(function (solution) {
@@ -37,7 +13,4 @@ function getSolution() {
         .fail(function (jqXHR, textStatus, err) {
             alert("maze not found");
         });
-
-
-    
 }
